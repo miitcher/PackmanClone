6ef51168ee9e7edc65c3b08db2ef86aad29c23d7
MiitcherONLaptop <mikael.gron@hotmail.com>
2016-12-16 15:20:44 +0200

    planning

---
+++ PackmanClone.py
@@ -0,0 +1,13 @@
+"""
+    Miitcher
+    For fun project doing a Packman clone.
+    
+    Python 3.4
+    PySide #
+"""
+
+
+# Handles window
+
+if __name__ == '__main__':
+    print("Vaka vaka vaka")
